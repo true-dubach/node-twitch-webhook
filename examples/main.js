5ed92a1d0cb04454729a2d649a37e9540cddb226
Roman Nikonov <niconneo@yandex.ru>
2019-03-23 17:01:15 +0300

    Changed examples/main.js so it's the same as in README

--- examples/main.js
+++ examples/main.js
@@ -1,46 +1,57 @@
 const TwitchWebhook = require('twitch-webhook')
 
-const clientId = process.env.CLIENT_ID
-if (!clientId) {
-  throw new Error('Twitch Client ID not provided')
-}
-const callback = process.env.CALLBACK
-if (!callback) {
-  throw new Error('Callback URL not provided')
-}
-
 const twitchWebhook = new TwitchWebhook({
-  client_id: clientId,
-  callback,
-  secret: 'hello human Kappa',
-  listen: {
-    autoStart: true
-  }
+    client_id: 'Your Twitch Client ID',
+    callback: 'Your Callback URL',
+    secret: 'It\'s a secret', // default: false
+    lease_seconds: 259200,    // default: 864000 (maximum value)
+    listen: {
+        port: 8080,           // default: 8443
+        host: '127.0.0.1',    // default: 0.0.0.0
+        autoStart: false      // default: true
+    }
 })
 
 // set listener for all topics
 twitchWebhook.on('*', ({ topic, options, endpoint, event }) => {
-  console.log(topic, options, endpoint, event)
+    // topic name, for example "streams"
+    console.log(topic)
+    // topic options, for example "{user_id: 12826}"
+    console.log(options)
+    // full topic URL, for example
+    // "https://api.twitch.tv/helix/streams?user_id=12826"
+    console.log(endpoint)
+    // topic data, timestamps are automatically converted to Date
+    console.log(event)
 })
 
-// subscribe to "users/follows" topic
-twitchWebhook.subscribe('users/follows', {
-  first: 1,
-  to_id: '12826' // ID of Twitch Chanell ¯\_(ツ)_/¯
+// set listener for topic
+twitchWebhook.on('users/follows', ({ event }) => {
+    console.log(event)
 })
-// subscribe to "streams" topic
-twitchWebhook.subscribe('streams', {
-  user_id: '12826'
+
+// subscribe to topic
+twitchWebhook.subscribe('users/follows', {
+    first: 1,
+    from_id: 12826 // ID of Twitch Channel ¯\_(ツ)_/¯
 })
 
 // renew the subscription when it expires
-twitchWebhook.on('unsubscibe', (obj) => {
+twitchWebhook.on('unsubscribe', (obj) => {
   twitchWebhook.subscribe(obj['hub.topic'])
 })
 
+// tell Twitch that we no longer listen
+// otherwise it will try to send events to a down app
 process.on('SIGINT', () => {
   // unsubscribe from all topics
   twitchWebhook.unsubscribe('*')
+
+  // or unsubscribe from each one individually
+  twitchWebhook.unsubscribe('users/follows', {
+    first: 1,
+    to_id: 12826
+  })
 
   process.exit(0)
 })
